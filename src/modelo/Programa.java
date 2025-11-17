91676f588c2384496525bbb4a8071a7587ef2523
Fran-casa <borrero.gufra25@triana.salesianos.edu>
2025-11-17 17:27:46 +0100

    Editado las dos clases

--- src/modelo/Programa.java
+++ src/modelo/Programa.java
@@ -2,7 +2,7 @@ package modelo;
 
 public class Programa {
 	public static void main(String[] args) {
-		Persona p = new Persona("Ana", 20);
+		Persona p = new Persona("Ana", 20, "DAM 1");
 		p.mostrarDatos();
 	}
 }
